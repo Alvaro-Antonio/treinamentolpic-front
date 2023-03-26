6b7ed970be12c1d7d713b776bc028cc92d84e5e9
Álvaro Antônio <alvaro.antonio@dcx.ufpb.br>
2023-03-25 23:55:54 -0300

    Construído a mecâncica para pegar as respostas dos simulados e mostrar no console

--- src/app/simulated/build-simulated/build-simulated.component.ts
+++ src/app/simulated/build-simulated/build-simulated.component.ts
@@ -1,10 +1,13 @@
-import { AfterViewInit, Component, EventEmitter, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, EventEmitter, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { catchError, map, startWith, switchMap } from 'rxjs';
 import { QuestionWithAlternatives } from '../questionWithAlternatives.model';
 import { SimulatedService } from '../simulated.service';
 import { merge, of as observableOf } from 'rxjs';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Question } from 'src/app/question/question.model';
+import { Alternative } from 'src/app/alternative/alternative.model';
+import { AnswerDTO } from 'src/app/simulated/simulated.model';
 
 @Component({
   selector: 'app-build-simulated',
@@ -17,18 +20,21 @@ export class BuildSimulatedComponent implements OnInit, AfterViewInit {
   public visibleFeedback = true;
 
   public form: FormGroup;
- 
 
   constructor(
     private repository : SimulatedService,
     private router : Router,
     private route: ActivatedRoute,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private cdr: ChangeDetectorRef
+
   ){
-    this.form = this.fb.group({
-      title: [null, Validators.required],
-      alternative : [null, Validators.required]
-    });
+    // this.form = this.fb.group({
+    //   title: [null, Validators.required],
+    //   alternative : [null, Validators.required]
+    // });
+    this.form = new FormGroup({});
+    
   }
  
   questionsWithAlternatives : QuestionWithAlternatives []= []
@@ -59,7 +65,17 @@ export class BuildSimulatedComponent implements OnInit, AfterViewInit {
       )
       .subscribe((data) => {
         this.questionsWithAlternatives = data;
+
+        const formControlsAlternatives  = this.questionsWithAlternatives.map((question) => {
+          return new FormControl(null, Validators.required);
+        });
+               
+        formControlsAlternatives.forEach((control, index) => {
+          this.form.addControl('answer' + index, control);
+        });
+        
       });
+      
     
   }
 
@@ -68,8 +84,24 @@ export class BuildSimulatedComponent implements OnInit, AfterViewInit {
   }
 
   postarDados() {
-    const dados = this.form.value;
-    console.log(dados);
+    const answers = [];
+    for (const key in this.form.controls) {
+      answers.push(this.form.controls[key].value);
+    }
+
+    console.log(answers);
+
+    this.visibleFeedback = false;
+    this.cdr.detectChanges();
+  }
+
+  convertFormControlInAnswerDTO(question : Question, alternative : Alternative) : AnswerDTO{
+    const resp : AnswerDTO = Object();
+    resp.question = question;
+    resp.alternativeSelected = alternative;
+
+    return resp;
+
   }
 
     
